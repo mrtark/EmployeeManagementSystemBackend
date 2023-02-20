5356a1d002c8510de22c416316249df9056947db
mrtark <murat_ark97@hotmail.com>
2023-02-20 14:52:12 +0300

    BaseEntity

---
+++ README.md
@@ -0,0 +1,65 @@
+
+---
+# Employee Management System Backend
+
+### Requirements
+* Java 11
+* intellij idea Ultimate
+---
+## RolMap
+### Bean
+```sh
+ModelMapper
+PasswordEncoder
+OpenApiConfiguration
+```
+
+### Exception
+```sh
+EmployeeManagementSystemException
+ResourceAuthorizedApiException
+ResourceNotFoundApiException
+ResourceCreatedApiException
+ResourceBadRequestApiException
+```
+
+###
+```sh
+RegisterDto
+BaseEntity
+RegisterEntity
+IRegisterRepository
+```
+
+# Tecnology
+#### BackEnd
+* javase
+* jdbc crud native
+* hibernate
+* spring core
+* spring data
+* spring rest
+* spring security
+---
+
+## Libraries
+* Lombok
+* Validation
+
+## Database
+* H2 Database
+* Mysql
+---
+## Dependency
+* Spring Web
+* Spring Data JPA
+* Spring Rest
+* Spring Security
+---
+# Spring Tools
+* Spring Boot DevTools
+* Spring Configuration Processor
+* Spring Boot Actuator
+---
+
+
---
+++ src/main/java/com/example/employee/data/entity/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.example.employee.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"created_date,update_date"},allowGetters = true)
+@MappedSuperclass
+
+abstract public class BaseEntity implements Serializable {
+    public static final Long serialVersionUID=1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false,insertable = true, nullable = false,unique = true)
+    private Long id;
+    @Column(name="system_created_date")
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date systemCreatedDate;
+    @CreatedBy
+    @Column(name = "createdBy",updatable = false)
+    private String createdBy;
+    @CreatedDate
+    @Column(name = "createdDate", updatable = false)
+    private String createdDate;
+    @LastModifiedBy
+    @Column(name = "updatedBy", updatable = false)
+    private String updatedBy;
+    @LastModifiedDate
+    @Column(name = "updatedDate",updatable = false)
+    private String updatedDate;
+
+}
