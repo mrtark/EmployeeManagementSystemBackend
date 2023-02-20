b8faf6c716003b5311d660d42c60b95da8c8f1a9
mrtark <murat_ark97@hotmail.com>
2023-02-20 15:12:07 +0300

    EmployeeEntity

---
+++ src/main/java/com/example/employee/data/entity/EmployeeEntity.java
@@ -0,0 +1,29 @@
+package com.example.employee.data.entity;
+
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "employeeT")
+public class EmployeeEntity extends BaseEntity implements Serializable {
+    public static final Long serialVersionUID=1L;
+    private String department;
+    private String gender;
+    private String username;
+    private String surname;
+    private String emailAdress;
+    private String telephoneNumber;
+    private String passwd;
+
+}
