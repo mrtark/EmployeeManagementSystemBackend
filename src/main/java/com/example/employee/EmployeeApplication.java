08f972a381d53d7458203356091eb66da371e4ee
mrtark <murat_ark97@hotmail.com>
2023-02-20 16:39:53 +0300

    TimeZone

--- src/main/java/com/example/employee/EmployeeApplication.java
+++ src/main/java/com/example/employee/EmployeeApplication.java
@@ -5,11 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = {
 		SecurityAutoConfiguration.class
 })
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
 public class EmployeeApplication {
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+	}
 
 	public static void main(String[] args) {
 		System.setProperty("java.awt.headless", "false");
